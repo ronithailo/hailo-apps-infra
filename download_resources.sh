4dcac2687564316e7079a9c902c4ea1cd2efa399
ronithailo <ronits@hailo.ai>
2025-01-25 08:06:36 +0000

    changed hef path

--- download_resources.sh
+++ download_resources.sh
@@ -18,11 +18,40 @@ download_model() {
   fi
 }
 
+download_model_new_h8() {
+  file_name=$(basename "$1")
+  if [ ! -f "$RESOURCE_DIR/hefs/hailo8/$file_name" ]; then
+    echo "Downloading $file_name..."
+    wget --tries=3 --retry-connrefused --quiet --show-progress "$1" -P "$RESOURCE_DIR/hefs/hailo8/" || {
+      echo "Failed to download $file_name after multiple attempts."
+      exit 1
+    }
+  else
+    echo "File $file_name already exists. Skipping download."
+  fi
+}
+
+download_model_new_h8l() {
+  file_name=$(basename "$1")
+  if [ ! -f "$RESOURCE_DIR/hefs/hailo8l/$file_name" ]; then
+    echo "Downloading $file_name..."
+    wget --tries=3 --retry-connrefused --quiet --show-progress "$1" -P "$RESOURCE_DIR/hefs/hailo8l/" || {
+      echo "Failed to download $file_name after multiple attempts."
+      exit 1
+    }
+  else
+    echo "File $file_name already exists. Skipping download."
+  fi
+}
+
 # Define all URLs in arrays
 H8_HEFS=(
   "https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.13.0/hailo8/yolov8m_pose.hef"
   "https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.13.0/hailo8/yolov5m_seg.hef"
   "https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.13.0/hailo8/yolov8m.hef"
+)
+
+H8_HEFS_NEW=(
   "https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.13.0/hailo8/scrfd_10g.hef"
   "https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.13.0/hailo8/arcface_mobilefacenet.hef"
 )
@@ -31,6 +60,9 @@ H8L_HEFS=(
   "https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov8s_h8l.hef"
   "https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov5n_seg_h8l_mz.hef"
   "https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov8s_pose_h8l.hef"
+)
+
+H8L_HEFS_NEW=(
   "https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/scrfd_2.5g.hef"
   "https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/arcface_mobilefacenet_h8l.hef"
 )
@@ -50,16 +82,28 @@ if [ "$1" == "--all" ]; then
   for url in "${H8_HEFS[@]}" "${H8L_HEFS[@]}" "${VIDEOS[@]}"; do
     download_model "$url" &
   done
+  for url in "${H8_HEFS_NEW[@]}"; do
+    download_model_new_h8 "$url" &
+  done
+  for url in "${H8L_HEFS_NEW[@]}"; do
+    download_model_new_h8l "$url" &
+  done
 else
   if [ "$DEVICE_ARCHITECTURE" == "HAILO8L" ]; then
     echo "Downloading HAILO8L models..."
     for url in "${H8L_HEFS[@]}"; do
       download_model "$url" &
     done
+    for url in "${H8L_HEFS_NEW[@]}"; do
+      download_model_new_h8l "$url" &
+    done
   elif [ "$DEVICE_ARCHITECTURE" == "HAILO8" ]; then
     echo "Downloading HAILO8 models..."
     for url in "${H8_HEFS[@]}"; do
       download_model "$url" &
+    done
+    for url in "${H8_HEFS_NEW[@]}"; do
+      download_model_new_h8 "$url" &
     done
   fi
 fi
